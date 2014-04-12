aa8e2be7b4f52a4080c2c6b973ec8652c7439fce
Nick Charlton <hello@nickcharlton.net>
2014-04-12 14:11:25 +0100

    Renames the example project to heatmap.

--- app/heatmap/views.py
+++ app/heatmap/views.py
@@ -4,9 +4,9 @@ from flask import Blueprint, render_template
 
 from app import db
 
-mod = Blueprint('example', __name__)
+mod = Blueprint('heatmap', __name__)
 
 
 @mod.route('/')
 def index():
-    return render_template("example/index.html")
+    return render_template("heatmap/index.html")
